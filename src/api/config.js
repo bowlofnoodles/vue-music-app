fca15bb4e70e0a01dceee0f72eacfe2540041169
bowlofnoodles <huahuazfh@qq.com>
2021-01-12 15:42:24 +0800

    refactor: 修改url

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const hostname = location.origin
+export const hostname = 'http://localhost:3000'
 // export const hostname = 'http://192.168.42.59'
 export const port = 3000
-export const URL = hostname + ':' + port
+export const URL = location.origin
