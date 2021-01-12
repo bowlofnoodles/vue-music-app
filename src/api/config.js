eb53197e0e873722149ba710f755316e689b87fb
bowlofnoodles <huahuazfh@qq.com>
2021-01-12 14:31:49 +0800

    refactor: 修改webpack配置

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const hostname = 'http://localhost'
+export const hostname = location.origin
 // export const hostname = 'http://192.168.42.59'
 export const port = 3000
 export const URL = hostname + ':' + port
