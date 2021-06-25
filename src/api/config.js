c83d2d4f38adff7b9fcee795e71b4951537fd27e
bowlofnoodles <huahuazfh@qq.com>
2021-06-25 18:03:43 +0800

    refactor: host

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,1 @@
-export const hostname = 'http://116.62.144.172'
-// export const hostname = 'http://192.168.42.59'
-export const port = 3000
-export const URL = hostname + ':' + port
+export const URL = 'https://music-server.bowlofnoodles.top/backend'
