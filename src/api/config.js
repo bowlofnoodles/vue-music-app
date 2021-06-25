4163ad7fe25e7b75bea7cb9e63a350097871e2c6
bowlofnoodles <huahuazfh@qq.com>
2021-06-25 17:06:40 +0800

    refactor: ip host

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-export const hostname = 'http://localhost'
+export const hostname = 'http://116.62.144.172'
 // export const hostname = 'http://192.168.42.59'
 export const port = 3000
 export const URL = hostname + ':' + port
